b8c99bc51d46cd37dbadf470b8ae10239083e188
Eli Joos <elijah2k@gmail.com>
2019-01-03 23:31:04 -0700

    Initial Commit

---
+++ goalpost-app/View/GoalCell.swift
@@ -0,0 +1,32 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Eli Joos on 1/3/19.
+//  Copyright © 2019 Eli Joos. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+    
+    
+    
+    
+   
+    
+    
+}
