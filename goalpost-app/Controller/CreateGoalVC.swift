f623077d190f838df4f8891bf72e4889470bd2a0
Eli Joos <elijah2k@gmail.com>
2019-01-12 00:11:42 -0700

    Built CreateGoalVC UI and set up buttons

---
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,41 @@
+//
+//  CreateGoalVC.swift
+//  goalpost-app
+//
+//  Created by Eli Joos on 1/12/19.
+//  Copyright © 2019 Eli Joos. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    
+    @IBOutlet weak var goalTextView: UITextView!
+    
+    @IBOutlet weak var shortTermBtn: UIButton!
+    
+    @IBOutlet weak var longTermBtn: UIButton!
+    
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
