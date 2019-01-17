d46b5fa8cdde6c40666ba5d52b773aed991371c0
Eli Joos <elijah2k@gmail.com>
2019-01-16 19:46:05 -0700

    130 - Saving Goal Data to Persistent Store in FinishGoalVC

--- goalpost-app/Controller/FinishGoalVC.swift
+++ goalpost-app/Controller/FinishGoalVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class FinishGoalVC: UIViewController, UITextFieldDelegate {
 
@@ -33,7 +34,49 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
 
     @IBAction func createGoalBtnWasPressed(_ sender: Any) {
         //Pass data into Core Data Goal Model
+        //Save our data
+        
+        if pointsTextField.text != nil {
+            
+            self.save { (complete) in
+                //code
+                dismiss(animated: true, completion: nil)
+            }
+        }
+        
     }
     
-
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismissDetail()
+    }
+    
+    func save(completion: (_ finished: Bool) -> ()) {
+    
+        //this is how we get our managed context
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        //create instance of goal, and given it a managed context that it needs to have so it KNOWS where it's SAVING THE DATA
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        //we need to pull the rawValue out!!! core data won't except an enum type
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        //as of now we haven't told it to save.
+       
+        do {
+            try managedContext.save()
+            
+            print("successfully saved data")
+            
+        } catch {
+            debugPrint("Could not save \(error.localizedDescription)")
+            
+        }
+        
+    }
+    
+    
 }
