48b9b5105370b3a0aaf52baabff2ea0e9c4903b8
Eli Joos <elijah2k@gmail.com>
2019-01-17 20:59:08 -0700

    133. Removing Objects from Persistent Store using UITableView Delete Action. All this was in GoalsVC because it just had to do with editing a row by deleting it then fetching the data again to refill it. I had an error where it crashed when I deleted a row because I was calling tableView.reloadData() during that process but I fixed it by deleting that.

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -29,6 +29,11 @@ class GoalsVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         //we are putting the fetch request in here because viewDidLoad will not be called if we go back to it from another view because it has already loaded it's just being presented
+        fetchCoreDataObjects()
+        tableView.reloadData()
+    }
+    
+    func fetchCoreDataObjects() {
         fetch { (complete) in
             //check to see if we should show or hide our tableview because if there's nothing in it we should hide it
             if goals.count >= 1 {
@@ -38,10 +43,7 @@ class GoalsVC: UIViewController {
             }
             
         }
-        
-        tableView.reloadData()
     }
-
 
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
         
@@ -72,7 +74,44 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
         cell.configureCell(goal: goal)
             return cell
             }
+    
+    //these funcs will allow us to delete rows by swiping
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+    
+    //the above function allows us to edit the cell. NOW we can call to editActionsForRowAt
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { (rowAction, indexPath) in
+            
+            // calling the function we created to delete a row and passing in the indexPath
+            self.removeGoal(atIndexPath: indexPath)
+            
+            //Now we need to fetch all of the tableview data again! SO IN VIEWDIDAPPEAR we need to do that whole FETCH PROCCESS AGAIN! So let's put that in it's own funciton 'fetchCoreDataObjects'
+            self.fetchCoreDataObjects()
+           
+            //deleting it
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            //we still havn't yet RETURNED IT to our tableview we just created it. But before we do that let's set a background color
+            
+           
+            
         }
+        
+        deleteAction.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        //RETURN OUR DELETE ACTION
+        return [deleteAction]
+        
+    }
+    
+    
+    
+}
 
 
 
@@ -94,5 +133,19 @@ extension GoalsVC {
             debugPrint("Could not fetch: \(error.localizedDescription)")
         }
         
+    }
+    
+    func removeGoal(atIndexPath indexPath: IndexPath) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        //pulling out the cell from the index path of the row
+        managedContext.delete(goals[indexPath.row])
+        
+        do {
+            try managedContext.save()
+            print("Successfully removed goal")
+        } catch {
+            print("Could not remove: \(error.localizedDescription)")
+        }
     }
 }
